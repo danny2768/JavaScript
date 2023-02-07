8df6103acabb903e5206b0e25df805d9add5bddd
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2023-02-07 18:14:43 -0500

    Run server comment

--- Angular/ts-intro/src/index.ts
+++ Angular/ts-intro/src/index.ts
@@ -2,3 +2,8 @@
     ===== Código de TypeScript =====
 */
 
+/*
+    Iniciar servidor: abrir: /home/danny2768/Documents/Data/Udemy/Angular/ts-intro
+    Ejecutar: npm start
+    Ingresar a http://localhost:8081/
+*/
