98b83847c9221fc84d9d9223554f108a1cf2de63
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2023-10-17 20:56:04 -0500

    17/10/2023: Fernando's code of section typescriptIntro added

--- Angular/ts-intro/src/index.ts
+++
@@ -1,9 +0,0 @@
-/*
-    ===== Código de TypeScript =====
-*/
-
-/*
-    Iniciar servidor: abrir: /home/danny2768/Documents/Data/Udemy/Angular/ts-intro
-    Ejecutar: npm start
-    Ingresar a http://localhost:8081/
-*/
