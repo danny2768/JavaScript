06a8060eecf92a86c57bf65a725bfbeb9fcdad55
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2024-01-11 23:41:57 -0500

    11/01/2024: 13-authApp

---
+++ Angular/13-authApp/src/app/dashboard/layouts/dasboard-layout/dasboard-layout.component.ts
@@ -0,0 +1,24 @@
+import { Component, computed, inject } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
+
+@Component({
+  templateUrl: './dasboard-layout.component.html',
+  styleUrls: ['./dasboard-layout.component.css']
+})
+export class DasboardLayoutComponent {
+
+  private authService = inject( AuthService );
+
+  public user = computed( () => this.authService.currentUser() );
+
+
+  // get user() {
+  //   return this.authService.currentUser();
+  // }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+
+}
