cf6090c68078e1c03e46f542af81cd013a971174
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2024-02-19 09:16:50 -0500

    19/02/2024: LogEntity

---
+++ Node/05-NOC/src/domain/entities/log.entity.ts
@@ -0,0 +1,20 @@
+
+export enum LogSeverityLevel {
+    low = 'low',
+    medium = 'medium',
+    high = 'high',
+}
+
+export class LogEntity {
+
+    public level: LogSeverityLevel;
+    public message: string;
+    public timestamp: Date;
+    
+    constructor( message: string, level: LogSeverityLevel){
+        this.message = message;
+        this.level = level;
+        this.timestamp = new Date();
+    }
+
+}
